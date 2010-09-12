3db143ecc0d631b8fd6eb70d8cdc2eca6ceb700e
shoebox639 <shoebox639@9a81ad78-4c58-4335-a12f-1e0ca0084a58>
2010-09-12 07:22:24 +0000

    getting rid of unused link functions
    git-svn-id: https://shumarks.svn.sourceforge.net/svnroot/shumarks/trunk@27 9a81ad78-4c58-4335-a12f-1e0ca0084a58

--- shumarks/app/controllers/links_controller.rb
+++ shumarks/app/controllers/links_controller.rb
@@ -1,33 +1,6 @@
 class LinksController < ApplicationController
   before_filter :login_required, :only => [:new, :create, :destroy, :index]
   
-  def new
-    @user = current_user
-  end
-
-  # Create a link for user currently signed in
-  def create
-    @user = current_user()
-    
-    if params[:l]
-      parameters = {:url => params[:l], :name => params[:n]}
-    else
-      parameters = params[:link]
-    end
-    
-    parameters[:is_viewed] = false
-
-    
-    @link = @user.links.build(parameters)
-    @link.save()
-    
-    if @link.errors.empty?()
-      redirect_to(user_queue_path)
-    else
-      render(:action => 'new')
-    end
-  end
-
   # Delete a link
   def delete
     @link = Link.find(params[:id])
