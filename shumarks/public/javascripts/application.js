64ee32414c38ceb568a08bbe0ca860f6433742da
shoebox639 <shoebox639@9a81ad78-4c58-4335-a12f-1e0ca0084a58>
2010-09-21 03:01:06 +0000

    refactored some css. refactored show/hide remove link
    git-svn-id: https://shumarks.svn.sourceforge.net/svnroot/shumarks/trunk@59 9a81ad78-4c58-4335-a12f-1e0ca0084a58

--- shumarks/public/javascripts/application.js
+++ shumarks/public/javascripts/application.js
@@ -1,3 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 document.cookie = 'tz' + '=' + new Date().getTimezoneOffset() + '; path=/'; 
+
+
+function showRemoveLink(index) {
+	$('#delete-link-td-' + index).show()
+}
+
+function hideRemoveLink(index) {
+	$('#delete-link-td-' + index).hide()
+}
+
