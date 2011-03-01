21e125d25191d27316c20c92a9eecb4d565738f5
shu <shoebox@gmail.com>
2011-02-28 22:04:55 -0500

    switched bookmarklet to new styles. skipped over the last step of the tutorial.

--- shumarks/config/environments/development.rb
+++ shumarks/config/environments/development.rb
@@ -16,5 +16,6 @@ config.action_controller.perform_caching             = false
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 
+#ENV['hostname'] = 'shumark.it'
 ENV['hostname'] = 'local.shumark.it'
 #ENV['hostname'] = 'localhost:3000'
